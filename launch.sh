3905bb0c4ececf65baeb01c92310e548d6458966
Saveliy Ermolenko <saveliy.ermolenko@acronis.com>
2020-03-06 15:41:42 +0300

    Initial commit

---
+++ launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Запуск Polybar со стандартным расположением конфигурационного файла в ~/.config/polybar/config
+polybar example 
